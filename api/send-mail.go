188cfadd99528ee47c310810487e0636009b254f
João Gabriel Klein <jgkl_25@hotmail.com>
2024-09-23 14:04:57 -0300

    feat: troca para handler

--- api/send-mail.go
+++ api/send-mail.go
@@ -1,25 +1,33 @@
-package main
+package handler
 
 import (
 	"log"
+	"mailer-microservice/config"
 	"mailer-microservice/controllers"
+	"mailer-microservice/middleware"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-// Handler é a função que será chamada pelo Vercel
-func Handler(c *fiber.Ctx) error {
-	// Chama o controlador para enviar o email
-	return controllers.SendMailHandler(c)
-}
-
-// Função principal para configurar e rodar o Fiber
-func main() {
+func Handler() {
+	// Cria uma nova instância do Fiber
 	app := fiber.New()
 
-	// Define a rota para enviar email
-	app.Post("/send-mail", Handler)
+	// Inicializa a configuração
+	config.Init()
 
-	// O Vercel espera que você retorne um manipulador HTTP
-	log.Fatal(app.Listen(":3000"))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to mailer!")
+	})
+	app.Use(middleware.AuthMiddleware)
+	app.Post("/send-mail", controllers.SendMailHandler)
+
+	log.Println("Server running")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3010"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 }
