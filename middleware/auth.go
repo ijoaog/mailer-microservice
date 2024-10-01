ec8df51ed2ecc21177e057acb010b73ee1d85e26
João Gabriel Klein <jgkl_25@hotmail.com>
2024-10-01 01:01:11 -0300

    feat: retirar Prints

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
@@ -10,25 +9,19 @@ import (
 
 func AuthMiddleware(c *fiber.Ctx) error {
 	token := c.Get("Authorization")
-	fmt.Println("Token recebido:", token)
 
 	if token == "" {
-		fmt.Println("Token ausente")
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 	}
 
 	parts := strings.Split(token, " ")
 	if len(parts) != 2 || parts[0] != "Bearer" {
-		fmt.Println("Formato do token inválido:", token)
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 	}
 
 	actualToken := parts[1]
-	fmt.Println("Token extraído:", actualToken)
-	fmt.Println("Token esperado:", os.Getenv("TOKEN"))
 
 	if actualToken == "" || actualToken != os.Getenv("TOKEN") {
-		fmt.Println("Token inválido ou ausente:", actualToken)
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
 	}
 
